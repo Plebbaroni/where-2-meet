7a7965a52d278374eef2b8448cc1bbcc090d3691
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-03-16 04:00:25 +1100

    🪖MEEERGE

--- frontend/src/Recommendations/Recommendations.tsx
+++ frontend/src/Recommendations/Recommendations.tsx
@@ -7,22 +7,25 @@ type RecommendationsProps = {
 }
 
 function Recommendations(props: RecommendationsProps) {
+	const places = Array.isArray(props.places) ? props.places : [];
   return (
-		<aside className={classes.container}>
-			<div className={classes.header}>
-				<Button
-					icon={ButtonIcons.RightChevrons}
-					type="button"
-				></Button>
-				<p className={classes.title}>Hangouts</p>
-			</div>
-			<ul className="hangouts">
-				{props.places.map(x => <li className={classes.rec}
-          >{x.displayName.text}
-        </li>)}
-			</ul>
 
-		</aside>
+	  <aside className={classes.container}>
+		<div className={classes.header}>
+		  <Button
+			icon={ButtonIcons.RightChevrons}
+			type="button"
+		  ></Button>
+		  <p className={classes.title}>Hangouts</p>
+		</div>
+		<ul className="hangouts">
+		  {places.map((x, index) => (
+			<li key={index} className={classes.rec}>
+			  {x.displayName.text}
+			</li>
+		  ))}
+		</ul>
+	  </aside>
 	);
 }
 
