b8d82fa36b7926800d22a8afac15aa2dea020d99
vincenttannos <tannos.vincent@gmail.com>
2025-03-16 03:33:41 +1100

    FEAT: Add Drawing Regions

--- frontend/src/Polygon/Polygon.tsx
+++ frontend/src/Polygon/Polygon.tsx
@@ -7,19 +7,7 @@ import {
 } from 'react';
 
 import type {Ref} from 'react';
-import {GoogleMapsContext, latLngEquals} from '@vis.gl/react-google-maps';
-import data from '../../../testData/isochrone';
-
-// type PolygonEventProps = {
-// //   onClick?: (e: google.maps.MapMouseEvent) => void;
-// //   onDrag?: (e: google.maps.MapMouseEvent) => void;
-// //   onDragStart?: (e: google.maps.MapMouseEvent) => void;
-// //   onDragEnd?: (e: google.maps.MapMouseEvent) => void;
-// //   onMouseOver?: (e: google.maps.MapMouseEvent) => void;
-// //   onMouseOut?: (e: google.maps.MapMouseEvent) => void;
-// //   onRadiusChanged?: (r: ReturnType<google.maps.Circle['getRadius']>) => void;
-// //   onCenterChanged?: (p: ReturnType<google.maps.Circle['getCenter']>) => void;
-// };
+import {GoogleMapsContext} from '@vis.gl/react-google-maps';
 
 export type PolygonProps = google.maps.PolygonOptions 
   // & PolygonEventProps;
@@ -27,7 +15,7 @@ export type PolygonProps = google.maps.PolygonOptions
 export type PolygonRef = Ref<google.maps.Polygon | null>;
 
 export interface Isochrone {
-  searchId: string;
+  search_id: string;
   shapes: Shape[];
   properties: {}
 };
@@ -39,57 +27,17 @@ export interface Coord {
 
 export interface Shape {
   shell: Coord[];
-  holes: Coord[];
+  holes: Coord[][];
 };
 
 function usePolygon(props: PolygonProps) {
-//   const {
-//     onClick,
-//     onDrag,
-//     onDragStart,
-//     onDragEnd,
-//     onMouseOver,
-//     onMouseOut,
-//     onRadiusChanged,
-//     onCenterChanged,
-//     radius,
-//     center,
-//     ...circleOptions
-//   } = props;
-  // This is here to avoid triggering the useEffect below when the callbacks change (which happen if the user didn't memoize them)
-//   const callbacks = useRef<Record<string, (e: unknown) => void>>({});
-//   Object.assign(callbacks.current, {
-//     onClick,
-//     onDrag,
-//     onDragStart,
-//     onDragEnd,
-//     onMouseOver,
-//     onMouseOut,
-//     onRadiusChanged,
-//     onCenterChanged
-//   });
-
   const polygon = useRef(new google.maps.Polygon({
     paths: props.paths,
     strokeColor: props.strokeColor,
-    strokeWeight: 10,
+    strokeWeight: 1,
     fillColor: props.fillColor,
     fillOpacity: 0.2,
 })).current;
-  // update circleOptions (note the dependencies aren't properly checked
-  // here, we just assume that setOptions is smart enough to not waste a
-  // lot of time updating values that didn't change)
-  // polygon.setOptions(polygonOptions);
-
-  // useEffect(() => {
-  //   if (!center) return;
-  //   if (!latLngEquals(center, circle.getCenter())) circle.setCenter(center);
-  // }, [center]);
-
-  // useEffect(() => {
-  //   if (radius === undefined || radius === null) return;
-  //   if (radius !== circle.getRadius()) circle.setRadius(radius);
-  // }, [radius]);
 
   const map = useContext(GoogleMapsContext)?.map;
 
@@ -108,39 +56,6 @@ function usePolygon(props: PolygonProps) {
       polygon.setMap(null);
     };
   }, [map]);
-
-  // attach and re-attach event-handlers when any of the properties change
-  // useEffect(() => {
-  //   if (!polygon) return;
-
-  //   // Add event listeners
-  //   const gme = google.maps.event;
-  //   [
-  //     ['click', 'onClick'],
-  //     ['drag', 'onDrag'],
-  //     ['dragstart', 'onDragStart'],
-  //     ['dragend', 'onDragEnd'],
-  //     ['mouseover', 'onMouseOver'],
-  //     ['mouseout', 'onMouseOut']
-  //   ].forEach(([eventName, eventCallback]) => {
-  //     gme.addListener(circle, eventName, (e: google.maps.MapMouseEvent) => {
-  //       const callback = callbacks.current[eventCallback];
-  //       if (callback) callback(e);
-  //     });
-  //   });
-  //   gme.addListener(circle, 'radius_changed', () => {
-  //     const newRadius = circle.getRadius();
-  //     callbacks.current.onRadiusChanged?.(newRadius);
-  //   });
-  //   gme.addListener(circle, 'center_changed', () => {
-  //     const newCenter = circle.getCenter();
-  //     callbacks.current.onCenterChanged?.(newCenter);
-  //   });
-
-  //   return () => {
-  //     gme.clearInstanceListeners(circle);
-  //   };
-  // }, [circle]);
 
   return polygon;
 }
